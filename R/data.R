4ce66a3cfe3046e284de1cc5a62df848d4830a5e
augustinlefevree <augustinroy91@gmail.com>
2026-04-16 09:28:38 +0200

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Jeu de données de football
+#'
+#' Ce jeu de données contient des matchs de football avec résultats et statistiques.
+#'
+#' @format Un data.frame avec plusieurs variables :
+#' \describe{
+#'   \item{Date}{Date du match}
+#'   \item{HomeTeam}{Équipe à domicile}
+#'   \item{AwayTeam}{Équipe à l'extérieur}
+#'   \item{FTHG}{Buts domicile}
+#'   \item{FTAG}{Buts extérieur}
+#'   \item{FTR}{Résultat du match}
+#' }
+#' @source Données football
+"df_football"
